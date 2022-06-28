980af6cf84cdc205f055c5fd56c627a9a5ee6b26
Nick Rockford <nwoffard@cnm.edu>
2022-06-28 09:40:00 -0600

    Added objects-interfaces-apis.js and finished this project for phase 2.

---
+++ objects-interfaces-apis.js
@@ -0,0 +1,34 @@
+/*
+ * Objects, Interfaces, and API's
+ */
+/* step 1 objects can contain data and functions
+ let dog = {
+     name: "Steve",
+     color: "brown",
+     breed: "Pug",
+     size: "Small",
+     bark: function(typeOfBark){
+         console.log("Bark!");
+     },
+ };
+
+ dog.bark();
+
+ //api stands for Application programming interface
+
+ */
+/* step 2passing by value and by reference
+
+function x(y){
+    y.num = y.num + 5;
+    console.log(y)
+}
+
+let y = {
+    name: "Tom",
+    num: 10,
+}
+x(y);
+console.log(y);
+
+ */

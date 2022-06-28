c9c4eeae564361e665f03eb2509622f6c5fe3a43
Nick Rockford <nwoffard@cnm.edu>
2022-06-27 22:25:27 -0600

    Trying to get this to run, going to upload this to the server to see if that fixes it.

---
+++ variables-and-data.js
@@ -0,0 +1,30 @@
+/*
+ * Variables, Data Types, and Typing
+ */
+
+let obj = {
+    key1: "value",
+    key2: 4,
+    boolean: true,
+    obj2: {
+        obj2Key1: "internal Object Value",
+    }
+};
+
+console.log(obj.key1);
+
+
+/*
+
+
+let wordData;
+
+wordData= "Word Data";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+ */

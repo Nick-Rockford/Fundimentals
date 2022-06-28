e6f1ab002a6d601711421bc9700407724c5abfc5
Nick Rockford <nwoffard@cnm.edu>
2022-06-27 22:25:27 -0600

    Trying to get this to run, going to upload this to the server to see if that fixes it.
    Ammend 1: Finally got it! I needed to just try running it from the run menu to select "basic run"

---
+++ variables-and-data.js
@@ -0,0 +1,61 @@
+/*
+ * Variables, Data Types, and Typing
+ */
+
+
+
+/* Fifth thing for this video, and damn im glad i learned the basics of java first. woo! this all made sense!
+*let add = 1 + 2;
+*let sub = 2-1 ;
+*let mult = 2 * 4;
+*let division = 4 / 2;
+*let mod = 5 % 2;
+*
+*let string = "String 1 " + "String 2";
+*
+*console.log(mod);
+*
+/*
+
+/* forth thing. basically the same thing as thing 3
+*let var1 = "2";
+*let var2 = 5;
+*let result = var1 + var2;
+*
+*console.log(result);
+*/
+
+/* Third thing. if you try to do an operation on an incompatible data type, JS will try to find a compatible type that does work.
+*let var1 = 2;
+*let var2 = 5;
+*let result = var1 + var2;
+*
+*console.log(result);
+*/
+
+/* This is the second thing they had me make
+*let obj = {
+*    key1: "Value 1",
+*    key2: 3,
+*    obj2: {
+*        obj2Key1 : "value of inner object",
+*    },
+*};
+*
+*console.log(obj.obj2.obj2Key1);
+*
+*/
+
+/* This is the first set of stuff they had me make, and then just erased it.
+
+let wordData;
+
+wordData= "Word Data";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+ */
